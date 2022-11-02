6e45da56f918957017cf112c7e5997c2ed6384f5
Arunjenson-SurveySparrow <116062066+Arunjenson-SurveySparrow@users.noreply.github.com>
2022-11-02 17:22:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const getElement = (selector) => {
+    const element = document.querySelector(selector)
+  
+    if (element) return element
+    throw Error(
+      `Please double check your class names, there is no ${selector} class`
+    )
+  }
+  
+  const links = getElement('.nav-list')
+  const navBtnDOM = getElement('.nav-btn')
+  
+  navBtnDOM.addEventListener('click', () => {
+    links.classList.toggle('show-links')
+  })
